15f2df48c8b12e5e3fde2eb73094e2100cc11974
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2025-03-03 13:54:58 +0100

    fix product grid in home page

--- client/src/components/home/ProductGridHome.tsx
+++ client/src/components/home/ProductGridHome.tsx
@@ -1,8 +1,10 @@
 import ProductCard from "../ProductCard";
-import productsData from "../../db.json";
+// import productsData from "../../db.json";
 import styles from "../../styles/ProductGridInHome.module.css";
-
+import { useGetProductsQuery } from "../../store/productApiSlice";
 const ProductGridHome = () => {
+  const { data: productsData } = useGetProductsQuery();
+  if (!productsData) return null;
   const selectedProducts = productsData.slice(0, 10);
   return (
     <section className={styles["product-contanier"]}>
@@ -10,7 +12,7 @@ const ProductGridHome = () => {
       <h2>Our Products</h2>
       <div className={styles["product-grid"]}>
         {selectedProducts?.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard key={product.id} product={product} viewType="grid" />
         ))}
       </div>
       <a href="/shop">Show More </a>
