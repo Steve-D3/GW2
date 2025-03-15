c391e6a64b6e63792baf41204a21146b77b9106b
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2025-03-15 10:17:54 +0100

    created authApi slice in store to handle register-signin-logout

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -4,18 +4,21 @@ import productApi from "./productApiSlice";
 import filterSlice from "./filterSlice";
 import addToCartSlice from "./addToCartSlice";
 
+import authApi from "./authApi";
+
 const store = configureStore({
   reducer: {
     signin: signinSlice,
     filter: filterSlice,
     [productApi.reducerPath]: productApi.reducer, 
   addToCart: addToCartSlice,
+  [authApi.reducerPath]: authApi.reducer,
   },
 
 
     
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(productApi.middleware),
+      getDefaultMiddleware().concat(productApi.middleware, authApi.middleware),
 
   },
 );
