088e34068c2b427a2e0d2cf7f6b65b8ba7be0006
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2025-03-19 09:14:28 +0100

    adj search styling

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,7 +1,14 @@
 import { useGetProductsQuery } from "../store/productApiSlice";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "../styles/SearchBar.module.css";
+import { toggelSearch } from "../store/searchSlice";
+import { useDispatch } from "react-redux";
 const ProductsList = () => {
+  const dispatch = useDispatch();
+  const handleCloseSearch = () => {
+    dispatch(toggelSearch());
+  };
   const { data: products, error, isLoading } = useGetProductsQuery();
 
   const [searchName, setSearchName] = useState("");
@@ -18,26 +25,24 @@ const ProductsList = () => {
   if (error) return <p>Error loading products.</p>;
 
   return (
-    <div>
+    <div className={styles["search-container"]}>
       <input
         type="text"
         value={searchName}
         onChange={handleSearchChange}
         placeholder="Search for products"
+        className={styles["search-bar"]}
       />
 
       {/* Show product suggestions only if there is a search query */}
       {searchName && (
-        <ul
-          style={{
-            listStyle: "none",
-            display: "grid",
-            backgroundColor: "white",
-          }}
-        >
+        <ul className={styles["product-suggestions"]}>
           {filteredProducts?.map((product) => (
-            <li key={product._id}>
-              <Link to={`/shop/${product._id}/${product.name}`}>
+            <li key={product._id} className={styles["product-suggestion"]}>
+              <Link
+                to={`/shop/${product._id}/${product.name}`}
+                onClick={handleCloseSearch}
+              >
                 <h3>{product.name}</h3>
                 <img
                   src={product.image_url[0]?.url}
